c28a0f7de3a89d4a7e5590f8b5ed715ff9c190ac
Heungsub Lee <sub@subl.ee>
2014-02-16 02:40:06 +0900

    fix freezing failure

--- freeze.py
+++ freeze.py
@@ -19,10 +19,5 @@ if os.path.exists('../gh-pages'):
 freezer = Freezer(app, with_static_files=False)
 
 
-@freezer.register_generator
-def static():
-    yield {'filename': 'logo-shadow.png'}
-
-
 if __name__ == '__main__':
     freezer.freeze()
