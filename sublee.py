91bf0d701adbab336f9970328fddc005030d725b
Heungsub Lee <sub@subl.ee>
2015-04-15 01:18:29 +0900

    Adjust <dl> style

--- sublee.py
+++ sublee.py
@@ -38,10 +38,10 @@ THEMES = os.path.join(ROOT, 'themes.yml')
 
 DEFAULT_THEME = 'sublee'
 MARKDOWN_EXTENSIONS = [
-    'markdown.extensions.attr_list',
     'markdown.extensions.def_list',
     'markdown.extensions.meta',
     'markdown.extensions.smarty',
+    'markdown_attr_plus',
 ]
 MINIFIERS = {
     'text/html': minify_html,
