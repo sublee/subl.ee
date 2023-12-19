40045c118f6009438dbb801e22a1e63b8b131c0a
Heungsub Lee <heungsub@subl.ee>
2023-12-20 01:52:14 +0900

    Fix XPath warning

--- sublee.py
+++ sublee.py
@@ -111,7 +111,7 @@ def render_icon(size: Union[int, Tuple[int, int]], radius: int = 0) -> str:
         width, height = size
 
     emblem = ElementTree.parse(ROOT/'artwork'/'emblem.svg')
-    emblem_path_commands = emblem.find('/{*}path').get('d')
+    emblem_path_commands = emblem.find('./{*}path').get('d')
 
     context = {
         'width': width,
